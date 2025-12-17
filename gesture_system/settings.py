5e759dfb67ce3880943f788577595b21813c9631
sanjay chudali <poudelsanjay93@gmail.com>
2025-12-17 18:36:18 +0545

    Add WhiteNoise middleware and storage configuration

--- gesture_system/settings.py
+++ gesture_system/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,3 +119,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise. storage.CompressedManifestStaticFilesStorage'
+
